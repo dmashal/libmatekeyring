a3e7d3add7064e730579626e32231bf73b1fd693
Dan Mashal <dan.mashal@fedoraproject.org>
2013-02-16 13:14:31 -0800

    Fix deprecated GStaticMutex

--- egg/egg-libgcrypt.c
+++ egg/egg-libgcrypt.c
@@ -52,28 +52,40 @@ fatal_handler (gpointer unused, int unknown, const gchar *msg)
 }
 
 static int
+#if GLIB_CHECK_VERSION(2, 31, 0)
+static GMutex thread_mutex;
 glib_thread_mutex_init (void **lock)
+#endif
 {
 	*lock = g_mutex_new ();
 	return 0;
 }
 
 static int
+#if GLIB_CHECK_VERSION(2, 31, 0)
+static GMutex thread_destroy;
 glib_thread_mutex_destroy (void **lock)
+#endif
 {
 	g_mutex_free (*lock);
 	return 0;
 }
 
 static int
+#if GLIB_CHECK_VERSION(2, 31, 0)
+static GMutex thread_lock;
 glib_thread_mutex_lock (void **lock)
+#endif
 {
 	g_mutex_lock (*lock);
 	return 0;
 }
 
 static int
+#if GLIB_CHECK_VERSION(2, 31, 0)
+static GMutex thread_unlock;
 glib_thread_mutex_unlock (void **lock)
+#endif
 {
 	g_mutex_unlock (*lock);
 	return 0;
